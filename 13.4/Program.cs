cbe83f6fae004b8a5d51b46c2860fbe4bae8c2f6
EdieLay <98664652+EdieLay@users.noreply.github.com>
2022-12-11 22:01:45 +0300

    4 и 5

--- 13.4/Program.cs
+++ 13.4/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Runtime.CompilerServices;
+
+
+object locker = new Object();
+Thread t1 = new Thread(Identify);
+Thread t2 = new Thread(Identify);
+Thread t3 = new Thread(Identify);
+t1.Start(1);
+Thread.Sleep(333);
+t2.Start(2);
+Thread.Sleep(333);
+t3.Start(3);
+t1.Join();
+t2.Join();
+t3.Join();
+
+void Identify(object obj)
+{
+    int id = (int)obj;
+    while (true)
+    {
+        Console.Write($"{id}-");
+        Thread.Sleep(TimeSpan.FromSeconds(1));
+    }
+}
+
--- 13.5/Program.cs
+++ 13.5/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Diagnostics;
+
+long sum, cur;
+object locker = new object();
+Stopwatch timer = new();
+for (int i = 1; i <= 100; i++)
+{
+    sum = 0;
+    cur = 1;
+    timer.Restart();
+    for (int j = 0; j < i; j++)
+    {
+        Thread thread = new Thread(Add)
+        {
+            Name = "Thread" + j
+        };
+        thread.Start();
+    }
+    Thread.Sleep(1000);
+    timer.Stop();
+    TimeSpan ts = timer.Elapsed;
+    Console.WriteLine($"Потоков: {i} --> Время: {ts.Milliseconds} --> Результат: {sum}");
+}
+
+void Add()
+{
+    while (cur <= 1000000)
+    {
+        lock (locker)
+        {
+            if (cur <= 1000000)
+            {
+                sum += cur;
+                cur++;
+            }
+        }
+    }
+}
